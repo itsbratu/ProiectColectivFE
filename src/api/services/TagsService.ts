cc320ffb26758071275cb26c6e1a6a1c552b4ca0
Mihai Dretcanu <mihai.dretcanu01@gmail.com>
2022-12-20 09:59:47 +0200

    Added tags api calls

--- src/api/services/TagsService.ts
+++ src/api/services/TagsService.ts
@@ -1,9 +1,40 @@
-import { Tag } from "../../models/tag";
+import {AddTagPayload, Tag, UpdateTagPayload} from "../../models/tag";
 import http from "./common";
 
 class TagsService {
   async getTags(token: string): Promise<Tag[]> {
     const result = await http.get<Tag[]>("/tags", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return result.data;
+  }
+
+  async addTag(payload: AddTagPayload, token: string): Promise<Tag> {
+    const result = await http.post<Tag>("/tags", JSON.stringify(payload), {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return result.data;
+  }
+
+  async editTag(payload: UpdateTagPayload, token: string): Promise<Tag> {
+    const result = await http.put<Tag>(
+      `/tags/${payload.id}`,
+      JSON.stringify(payload),
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return result.data;
+  }
+
+  async deleteTag(tagId: string, token: string): Promise<Tag> {
+    const result = await http.delete<Tag>(`/tags/${tagId}`, {
       headers: {
         Authorization: `Bearer ${token}`,
       },
